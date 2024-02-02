f1c6068fa11b8b8c9dec96f0f58c9d7474ff8db0
giovanni <giovanni.cornacchia74@gmail.com>
2024-02-02 19:49:39 +0100

    2.0, serve solo il collegamento a firebase e il controllo nel db

--- app/src/main/java/com/example/scanner/MainActivity.java
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.scanner;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -22,31 +24,50 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Setup Scan Options
-        ScanOptions options = new ScanOptions();
-        options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES);
-        options.setPrompt("");
-        options.setCameraId(0);  // Use a specific camera of the device
-        options.setBeepEnabled(false);
-        options.setBarcodeImageEnabled(true);
-        options.setOrientationLocked(false);
-        barcodeLauncher.launch(options);
+        Button scanButton = findViewById(R.id.button);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Setup Scan Options
+                ScanOptions options = new ScanOptions();
+                options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES);
+                options.setPrompt("");
+                options.setCameraId(0);  // Use a specific camera of the device
+                options.setBeepEnabled(false);
+                options.setBarcodeImageEnabled(true);
+                options.setOrientationLocked(false);
+                barcodeLauncher.launch(options);
+            }
+
+            // Setup intent for Scan
+            private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+                if (result.getContents() != null) {
+                    Toast.makeText(MainActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+        });
+
     }
 
-    // Setup intent for Scan
-    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(), result -> {
-        if (result.getContents() == null) {
-            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-            TextView myTextView = findViewById(R.id.textView2);
-            myTextView.setText(result.getContents());
+    //Squaring logo
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // Get the width of the ImageView
+            ImageView logoImageView = findViewById(R.id.imageView);
+
+            // Set the height of the ImageView to match its width
+            logoImageView.getLayoutParams().height = logoImageView.getWidth();
+            logoImageView.requestLayout();
         }
-    });
+    }
+
 }
